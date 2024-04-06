e31d0360e89b7a9c06d1c56d559c2cefc88654c9
Koki Takahashi <hakatasiloving@gmail.com>
2024-04-06 18:20:10 +0900

    Setup lint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,32 +1,17 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
-  },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
-  rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2],
-  },
+	extends: '@hakatashi/eslint-config/typescript',
+	plugins: [
+		'canonical',
+	],
+	rules: {
+		'import/prefer-default-export': 'off',
+		camelcase: 'off',
+		'no-unused-vars': 'off',
+		'no-dupe-class-members': 'off',
+		'import/default': 'off',
+		'import/no-namespace': 'off',
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'canonical/require-extension': 'error',
+	},
 };
